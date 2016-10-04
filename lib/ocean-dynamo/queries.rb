ee7c5b1627668d2a5c62ec3cb106e3adc8db481c
generalmilk <generalmilk@gmail.com>
2016-10-04 19:05:11 -0400

    add rate control

--- lib/ocean-dynamo/queries.rb
+++ lib/ocean-dynamo/queries.rb
@@ -1,6 +1,6 @@
 module OceanDynamo
   module Queries
-    rate_per_sec = 100
+    @rate_per_sec = 100
 
     # ---------------------------------------------------------
     #
@@ -183,7 +183,7 @@ module OceanDynamo
       hash_value = hash_value.to_i if hash_value.is_a?(Time)
       range_value = range_value.to_i if range_value.is_a?(Time)
       options = condition_builder(hash_key, hash_value, range_key, comparator, range_value,
-                                  limit: limit ? [limit,rate_per_sec].min : nil, scan_index_forward: scan_index_forward)
+                                  limit: limit ? [limit,@rate_per_sec].min : nil, scan_index_forward: scan_index_forward)
       index_name = (range_key ? "#{hash_key}_#{range_key}" : hash_key.to_s) + "_global"
       options[:index_name] = index_name
       raise "Undefined global index: #{index_name}" unless global_secondary_indexes[index_name]
@@ -242,7 +242,7 @@ module OceanDynamo
       range_value = range_value.to_i if range_value.is_a?(Time)
       options = condition_builder(hash_key, hash_value, range_key, comparator, range_value,
                                   select: count ? :count : :all_attributes,
-                                  limit: limit ? [limit,rate_per_sec].min : nil, scan_index_forward: scan_index_forward,
+                                  limit: limit ? [limit,@rate_per_sec].min : nil, scan_index_forward: scan_index_forward,
                                   consistent: consistent)
       index_name = range_key.to_s
       options[:index_name] = index_name
@@ -289,7 +289,7 @@ module OceanDynamo
       range_value = range_value.to_i if range_value.is_a?(Time)
       options = condition_builder(hash_key, hash_value, range_key, comparator, range_value,
                                   select: count ? :count : :all_attributes,
-                                  limit: limit ? [limit,rate_per_sec].min : nil, scan_index_forward: scan_index_forward,
+                                  limit: limit ? [limit,@rate_per_sec].min : nil, scan_index_forward: scan_index_forward,
                                   consistent: consistent)
       
       in_batches :query, options do |attrs|
